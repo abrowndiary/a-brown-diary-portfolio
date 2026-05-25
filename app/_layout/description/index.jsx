d4e5da8a269b44a2e209a80d79f5bb84faeb2afd
abrowndiary <abrowndiary@gmail.com>
2026-05-25 18:46:31 +0200

    Update index.jsx

--- app/_layout/description/index.jsx
+++ app/_layout/description/index.jsx
@@ -9,7 +9,7 @@ import { MagneticButton, ParallaxFade, ParallaxReveal } from '@/components';
 import { Title, Wrapper } from './index.styled';
 
 const phrase =
-  'Helping brands to stand out in the digital era. Together we will set the new status quo. No nonsense, always on the cutting edge.';
+  'I worked in digital transformation, AI implementation, cybersecurity and automation globally. Built life in scratch in Germany and a startup. This diary is where I share what actually works — and what doesn’t';
 
 export function Description() {
   return (
@@ -24,8 +24,8 @@ export function Description() {
         <div className='basis-7/12 lg:basis-3/12'>
           <ParallaxFade>
             <Balancer as='p' className='mt-2 text-base lg:text-lg'>
-              The combination of my passion for design, code & interaction
-              positions me in a unique place in the web design world.
+              The combination of my passion for Physical AI, cybersecurity , and digital transformation
+              positions me in a unique place in the tech world.
             </Balancer>
           </ParallaxFade>
         </div>
